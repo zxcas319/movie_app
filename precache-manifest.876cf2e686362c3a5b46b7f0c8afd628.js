9f7b12e1ed49cddf64982d579f53d2a07f39928f
zxcas319 <zxcas319@ajou.ac.kr>
2018-10-28 03:34:47 +0900

    Updates

---
+++ precache-manifest.876cf2e686362c3a5b46b7f0c8afd628.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "67062a848f312440e89f",
+    "url": "/movie_app/static/js/main.67062a84.chunk.js"
+  },
+  {
+    "revision": "392d6e18dd1fb16cb5e2",
+    "url": "/movie_app/static/js/1.392d6e18.chunk.js"
+  },
+  {
+    "revision": "67062a848f312440e89f",
+    "url": "/movie_app/static/css/main.b868f43c.chunk.css"
+  },
+  {
+    "revision": "f47e038ffa727d9d8dcad9cacc4d4295",
+    "url": "/movie_app/index.html"
+  }
+];
