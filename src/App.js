65733cee364d12ad04ac144aeb85a21902823a09
zxcas319 <zxcas319@ajou.ac.kr>
2018-10-28 03:21:22 +0900

    gh-pages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Movie from './Movie';
+
+
+
+
 
 class App extends Component {
+ 
+  state={}
+
+  componentDidMount(){
+    this._getMovies(); 
+  }
+  _renderMovies= ()=>{
+    const movies=this.state.movies.map(movie => {
+      return (
+      <Movie 
+      title={movie.title_english}
+      poster={movie.medium_cover_image}
+      key={movie.id}
+      genres={movie.genres} 
+      synopsis={movie.synopsis}
+      /> 
+      );
+    })
+    return movies;
+  };
+  
+   _getMovies= async()=>{
+    const movies = await this._callApi();// 이작업이 끝나야 밑에가 실행
+    this.setState({
+      movies
+    });
+   };
+
+   _callApi=()=>{
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(potato=>potato.json())
+    .then(json=>json.data.movies)//=> 리턴의 뜻이 내재
+    .catch(err=>console.log(err))
+   };
+  
   render() {
+    const { movies } = this.state;
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={movies ? "App" : "App--loading"}>
+        {movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
